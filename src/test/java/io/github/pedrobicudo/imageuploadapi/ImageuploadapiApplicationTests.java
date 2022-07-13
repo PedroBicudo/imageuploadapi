bc95e770e47ebc445ec049d6b8a2abc6edab65f5
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-07-12 21:07:51 -0300

    test: add @TestContainers and @Container

--- src/test/java/io/github/pedrobicudo/imageuploadapi/ImageuploadapiApplicationTests.java
+++ src/test/java/io/github/pedrobicudo/imageuploadapi/ImageuploadapiApplicationTests.java
@@ -1,10 +1,30 @@
 package io.github.pedrobicudo.imageuploadapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 class ImageuploadapiApplicationTests {
+
+	@Container
+	private static final PostgreSQLContainer container = new PostgreSQLContainer("postgres:14.2-alpine");
+
+	@DynamicPropertySource
+	public static void overrideProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", () -> {
+			return container.getJdbcUrl()+"?currentSchema=public";
+		});
+		registry.add("spring.datasource.username", container::getUsername);
+		registry.add("spring.datasource.password", container::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
